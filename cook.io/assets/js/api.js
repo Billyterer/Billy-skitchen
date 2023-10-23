860321964715a2a692b13a765b12812ce17a25ad
Billyterer <141916398+Billyterer@users.noreply.github.com>
2023-10-23 14:55:57 +0300

    Add files via upload

---
+++ cook.io/assets/js/api.js
@@ -0,0 +1,31 @@
+
+"use strict";
+"e9842eb9";
+
+
+
+window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
+const /** {String} */ APP_ID = "e9842eb9";
+const /** {String} */ API_KEY = "fa9b4805565e0d820c6cb9828f753600";
+const /** {String} */ TYPE = "public";
+
+/**
+ * @param {Array} queries Query array
+ * @param {Function} successCallback Success callback function
+ */
+
+export const fetchData = async function (queries, successCallback) {
+  const /** {String} */ query = queries?.join("&")
+    .replace(/,/g, "=")
+    .replace(/ /g, "%20")
+    .replace(/\+/g, "%2B");
+
+  const /** {String} */ url = `${ACCESS_POINT}?app_id=${APP_ID}&app_key=${API_KEY}&type=${TYPE}${query ? `&${query}` : ""}`;
+
+  const /** {Object} */ response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    successCallback(data);
+  }
+}
